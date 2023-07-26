afc3bcb43567ca6619864c5774bd5087a4060db8
JOHN-AMAO <amaojohn767@gmail.com>
2023-07-25 22:29:07 -0700

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 px-5 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I&apos;m a software developer with experience in TypeScript and
+        I&apos;m a skilled software developer with experience in TypeScript and
         JavaScript, and expertise in frameworks like React, Node.js, and
         Three.js. I&apos;m a quick learner and collaborate closely with clients
         to create efficient, scalable and user-friendly solutions that solve
