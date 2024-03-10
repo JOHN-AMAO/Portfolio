0f7b760cb8f525eea50e983da82637f3319b2583
JOHN-AMAO <amaojohn767@gmail.com>
2024-03-10 22:37:09 +0100

    editor.jsx

--- components/Editor.jsx
+++ components/Editor.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useRef, useState } from "react";
 import EditorJs from "@editorjs/editorjs";
 import Header from "@editorjs/header";
 import List from "@editorjs/list";
@@ -7,6 +8,13 @@ import Code from "@editorjs/code";
 import LinkTool from "@editorjs/link";
 import Embed from "@/editorjs/embed";
 import InlineCode from "@editorjs/inlinecode";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { Post } from "@prisma/client";
+import { useForm } from "react-hook-form";
+import TextareaAutosize from "react-textarea-autosize";
+import "@/styles/editor.css";
+import { cn } from "@/lib/utils";
 
 const editor = new EditorJs({
   holder: "editorjs",
@@ -22,6 +30,11 @@ const editor = new EditorJs({
 });
 
 const Editor = () => {
+  const ref = useRef();
+  const router = useRouter();
+  const [isSaving, setIsSaving] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
+
   return <div>Editor</div>;
 };
 
