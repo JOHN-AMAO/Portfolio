fb563906fe2fabf1e4829dd37e84290f1b1d0666
JOHN-AMAO <amaojohn767@gmail.com>
2024-03-12 18:28:14 +0100

    ui changes for projects page

--- app/(routes)/projects/page.tsx
+++ app/(routes)/projects/page.tsx
@@ -3,9 +3,10 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className='flex flex-col gap-6 justify-center text-white items-center md:ml-20 md:mt-[-50px]'>
+    <div className='flex flex-col gap-6 justify-center text-white items-center md:ml-20 '>
       <div className=' flex flex-col w-3/4 p-5'>
-        <h1 className='text-4xl mb-4 p-4 rounded font-serif font-extrabold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 '>
+        <h1 className='text-xl md:text-4xl mt-5 mb-5'>Projects.</h1>
+        <h1 className='text-lg md:text-4xl mb-4 p-4 rounded font-serif font-extrabold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 '>
           Major Projects I&apos;ve worked on:
         </h1>
         <p className='font-bold text-gray-500'>
