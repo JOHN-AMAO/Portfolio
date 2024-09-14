4818ab80f5aa8cd70d516fd8dd846064c891333b
JOHN-AMAO <amaojohn767@gmail.com>
2024-09-14 02:12:59 -0700

    .

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -27,6 +27,13 @@ const MobileNav = () => {
     return pathname?.startsWith(href) ?? false;
   };
 
+  // Check if the current path is a blog post
+  const isBlogPost = pathname?.startsWith('/blog/') && pathname !== '/blog';
+
+  if (isBlogPost) {
+    return null; // Don't render the nav on blog post pages
+  }
+
   return (
     <div className='md:hidden fixed bottom-0 left-0 right-0 shadow-md bg-white backdrop-filter backdrop-blur-xl bg-opacity-10'>
       <div className='flex flex-row justify-evenly w-full py-2'>
